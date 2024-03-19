14b58a44d4a8e86ac783d738239fb336e6386564
0051Nasrulhakim <ti192400051@gmail.com>
2024-03-19 21:18:56 +0700

    updated

--- src/app/categori/game/ml/page.js
+++ src/app/categori/game/ml/page.js
@@ -4,7 +4,7 @@ export default function Ml() {
 
   return (
     <>
-      <div className='produk-ml'>
+      <div className='produk-game'>
 
         <div className='section-left'>
           asalskalsk
